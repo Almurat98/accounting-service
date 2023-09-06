5b079d01a45d823ecd7623527611061d3f242abd
Almurat98 <almurat908@gmail.com>
2023-09-06 12:31:03 -0500

    category service methods added

--- src/main/java/com/cydeo/service/impl/CategoryServiceImpl.java
+++ src/main/java/com/cydeo/service/impl/CategoryServiceImpl.java
@@ -1,9 +1,31 @@
 package com.cydeo.service.impl;
 
+import com.cydeo.dto.CategoryDto;
 import com.cydeo.service.CategoryService;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class CategoryServiceImpl implements CategoryService {
 
+    @Override
+    public List<CategoryDto> getAllCategories() {
+        return null;
+    }
+
+    @Override
+    public void create(CategoryDto dto) {
+
+    }
+
+    @Override
+    public void edit(CategoryDto dto) {
+
+    }
+
+    @Override
+    public void delete(Long id) {
+
+    }
 }
