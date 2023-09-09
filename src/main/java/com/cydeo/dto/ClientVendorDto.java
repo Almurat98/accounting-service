3fba234044f17ab3cd3f886200326fba0def3c6a
asliya2001 <asliyaabdulloeva@gmail.com>
2023-09-09 21:50:17 +0200

    Added ClientVendor entity, dto and enum

--- src/main/java/com/cydeo/dto/ClientVendorDto.java
+++ src/main/java/com/cydeo/dto/ClientVendorDto.java
@@ -1,4 +1,28 @@
 package com.cydeo.dto;
 
+import com.cydeo.enums.ClientVendorType;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class ClientVendorDto {
+
+    private Long id;
+
+    private String clientVendorName;
+
+    private String phone;
+
+    private String website;
+
+    ClientVendorType clientVendorType;
+
+    AddressDto address;
+
+    CompanyDto company;
+
+    boolean hasInvoice;
 }
--- src/main/java/com/cydeo/entity/ClientVendor.java
+++ src/main/java/com/cydeo/entity/ClientVendor.java
@@ -1,9 +1,36 @@
 package com.cydeo.entity;
 
 import com.cydeo.entity.common.BaseEntity;
+import com.cydeo.enums.ClientVendorType;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.Where;
 
-import javax.persistence.Entity;
+import javax.persistence.*;
 
 @Entity
+@Table(name = "clients_vendors")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Where(clause = "is_deleted = false")
 public class ClientVendor extends BaseEntity {
+
+    private String clientVendorName;
+
+    private String phone;
+
+    private String website;
+
+    @Enumerated(EnumType.STRING)
+    ClientVendorType clientVendorType;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "address_id")
+    Address address;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "company_id")
+    Company company;
 }
